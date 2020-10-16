9925727dc767230e1389a2c50036d727581d3619
AlexeyRyjkov <kawai4ik98@gmail.com>
2020-10-16 21:13:02 +0300

    Добавьте файлы проекта.

---
+++ Clock/myWatch.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Clock
+{
+    partial class myWatch
+    {
+        /// <summary> 
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary> 
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.colorDialog2 = new System.Windows.Forms.ColorDialog();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.BackColor = System.Drawing.Color.Transparent;
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.button3, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button4, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 269);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(300, 31);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.AutoSize = true;
+            this.button3.Location = new System.Drawing.Point(222, 5);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Обводка";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button4.AutoSize = true;
+            this.button4.Location = new System.Drawing.Point(3, 5);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "Циферблат";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // myWatch
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.DoubleBuffered = true;
+            this.Name = "myWatch";
+            this.Size = new System.Drawing.Size(300, 300);
+            this.Load += new System.EventHandler(this.myWatch_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Clock_Paint);
+            this.Resize += new System.EventHandler(this.myWatch_Resize);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.ColorDialog colorDialog2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
---
+++ Clock/myWatch.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Linq.Expressions;
+using System.Reflection.Emit;
+
+namespace Clock
+{
+    public partial class myWatch : UserControl
+    {
+        const float PI = 3.14F;
+
+        float fRadius, fCenterX, fCenterY, fCenterCircleRadius, fHourLength;
+        float fMinLength, fSecLength, fHourThickness, fMinThickness, fSecThickness;
+        bool bDraw5MinuteTicks = true;
+        bool bDraw1MinuteTicks = true;
+        float fTicksThickness = 2;
+
+        Color hrColor = Color.Red;
+        Color minColor = Color.Black;
+        Color secColor = Color.Black;
+
+        Color circleColor = Color.Black;
+        Color ticksColor = Color.Green;
+
+        SolidBrush numberic = new SolidBrush(Color.White);
+        SolidBrush cant = new SolidBrush(Color.Black);
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+                cant.Color = colorDialog1.Color;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (colorDialog2.ShowDialog() == DialogResult.OK)
+                ticksColor = colorDialog2.Color;
+        }
+
+        private DateTime time;
+
+        public myWatch()
+        {
+            InitializeComponent();
+        }
+
+        private void myWatch_Resize(object sender, EventArgs e)
+        {
+            this.Width = this.Height;
+            this.fRadius = this.Height / 2;
+            this.fCenterX = this.ClientSize.Width / 2;
+            this.fCenterY = this.ClientSize.Height / 2;
+            this.fHourLength = (float)this.Height / 3 / 1.85F;
+            this.fMinLength = (float)this.Height / 3 / 1.20F;
+            this.fSecLength = (float)this.Height / 3 / 1.15F;
+            this.fHourThickness = (float)this.Height / 100;
+            this.fMinThickness = (float)this.Height / 150;
+            this.fSecThickness = (float)this.Height / 200;
+            this.fCenterCircleRadius = this.Height / 50;
+            timer1.Start();
+        }
+        private void myWatch_Load(object sender, EventArgs e)
+        {
+            time = DateTime.Now;
+            this.myWatch_Resize(sender, e);
+        }
+
+        private void Clock_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.FillEllipse(cant, 0,0, this.Width, this.Height);
+            e.Graphics.FillEllipse(numberic, 6,6,this.Width-12,this.Height-12);
+            float fRadHr = (time.Hour % 12 + time.Minute / 60F) * 30 * PI / 180;
+            float fRadMin = (time.Minute) * 6 * PI / 180;
+            float fRadSec = (time.Second) * 6 * PI / 180;
+
+            DrawLine(this.fHourThickness, this.fHourLength, hrColor, fRadHr, e);
+            DrawLine(this.fMinThickness, this.fMinLength, minColor, fRadMin, e);
+            DrawLine(this.fSecThickness, this.fSecLength, secColor, fRadSec, e);
+
+
+            for (int i = 0; i < 60; i++)
+            {
+                if (this.bDraw5MinuteTicks == true && i % 5 == 0) // Draw 5 minute ticks
+                {
+                    e.Graphics.DrawLine(new Pen(ticksColor, fTicksThickness),
+                    fCenterX + (float)(this.fRadius / 1.50F * Math.Sin(i * 6 * PI / 180)),
+                    fCenterY - (float)(this.fRadius / 1.50F * Math.Cos(i * 6 * PI / 180)),
+                    fCenterX + (float)(this.fRadius / 1.65F * Math.Sin(i * 6 * PI / 180)),
+                    fCenterY - (float)(this.fRadius / 1.65F * Math.Cos(i * 6 * PI / 180)));
+                }
+                else if (this.bDraw1MinuteTicks == true) // draw 1 minute ticks
+                {
+
+                    e.Graphics.DrawLine(new Pen(ticksColor, fTicksThickness),
+                    fCenterX + (float)(this.fRadius / 1.50F * Math.Sin(i * 6 * PI / 180)),
+                    fCenterY - (float)(this.fRadius / 1.50F * Math.Cos(i * 6 * PI / 180)),
+                    fCenterX + (float)(this.fRadius / 1.55F * Math.Sin(i * 6 * PI / 180)),
+                    fCenterY - (float)(this.fRadius / 1.55F * Math.Cos(i * 6 * PI / 180)));
+                }
+            }
+
+            //draw circle at center
+            e.Graphics.FillEllipse(new SolidBrush(circleColor), 
+                fCenterX - fCenterCircleRadius / 2, fCenterY - fCenterCircleRadius / 2, 
+                fCenterCircleRadius, fCenterCircleRadius);
+            this.myWatch_Resize(sender, e);
+        }
+
+        private void DrawLine(float fThickness, float fLength, Color color, float fRadians, PaintEventArgs e)
+        {
+            e.Graphics.DrawLine(new Pen(color, fThickness),
+            fCenterX - (float)(fLength / 9 * Math.Sin(fRadians)),
+            fCenterY + (float)(fLength / 9 * Math.Cos(fRadians)),
+            fCenterX + (float)(fLength * Math.Sin(fRadians)),
+            fCenterY - (float)(fLength * Math.Cos(fRadians)));
+
+        }
+        private void DrawPolygon(float fThickness, float fLength, Color color, float fRadians, PaintEventArgs e)
+        {
+
+            PointF A = new PointF((float)(fCenterX + fThickness * 2 * Math.Sin(fRadians + PI / 2)),
+            (float)(fCenterY - fThickness * 2 * Math.Cos(fRadians + PI / 2)));
+            PointF B = new PointF((float)(fCenterX + fThickness * 2 * Math.Sin(fRadians - PI / 2)),
+            (float)(fCenterY - fThickness * 2 * Math.Cos(fRadians - PI / 2)));
+            PointF C = new PointF((float)(fCenterX + fLength * Math.Sin(fRadians)),
+            (float)(fCenterY - fLength * Math.Cos(fRadians)));
+            PointF D = new PointF((float)(fCenterX - fThickness * 4 * Math.Sin(fRadians)),
+            (float)(fCenterY + fThickness * 4 * Math.Cos(fRadians)));
+            PointF[] points = { A, D, B, C };
+            e.Graphics.FillPolygon(new SolidBrush(color), points);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            time = DateTime.Now;
+            this.Refresh();
+        }
+
+    }
+}
